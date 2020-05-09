b782e6eee63b2c1d5ee106486c94e7af83e86742
petr.osipov91@mail.ru <petr.osipov91@mail.ru>
2020-05-09 22:06:37 +0600

    Добавлен класс отвечающий за чтение данных из файла

---
+++ Alchemistry/Assets/Scripts/Auxiliary/JsonReader.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class JsonReader<T> : IReader<T>
+{
+	public T Read(string filePath)
+	{
+		using (StreamReader streamReader = new StreamReader(filePath))
+		{
+			string file = streamReader.ReadToEnd();
+			return JsonUtility.FromJson<T>(file);
+		}
+	}
+}
